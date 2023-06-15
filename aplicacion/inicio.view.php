93a0a9e7e920aa40e4fb00b04b516793f3be5817
Davidjr16 <davidrojasjr16@gmail.com>
2023-06-15 07:57:57 -0500

    Agrego Adelanto Sistema De Notas

---
+++ aplicacion/inicio.view.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<?php
+require 'functions.php';
+$permisos = ['Administrador','Profesor','Acudiente'];
+permisos($permisos);
+
+?>
+<html>
+<head>
+<title>Inicio | Sistema de Notas</title>
+    <meta name="description" content="Registro de Notas del Centro Escolar Profesor Lennin" />
+    <link rel="stylesheet" href="css/style.css" />
+
+</head>
+<body>
+<div class="header">
+        <center><h1>Sistema de Notas</h1></center>
+        <h3>Usuario:  <?php echo $_SESSION["username"] ?></h3>
+</div>
+<nav>
+    <ul>
+        <li class="active"><a href="inicio.view.php">Inicio</a> </li>
+        <li><a href="alumnos.view.php">Registro de Alumnos</a> </li>
+        <li><a href="listadoalumnos.view.php">Listado de Alumnos</a> </li>
+        <li><a href="notas.view.php">Registro de Notas</a> </li>
+        <li><a href="listadonotas.view.php">Consulta de Notas</a> </li>
+        <li class="right"><a href="logout.php">Salir</a> </li>
+
+    </ul>
+</nav>
+
+<div class="body">
+    <div class="panel">
+           <h1 class="text-center">Centro De Formacion Agroindustrial "La Angostura"</h1>
+        <?php
+        if(isset($_GET['err'])){
+            echo '<h3 class="error text-center">ERROR: Usuario no autorizado</h3>';
+        }
+        ?>
+        <br>
+        <hr>
+        <p class="text-center"><strong>Integrantes</strong><br><br>David Rojas<br>Mary Aldana</p>
+        <br>
+        </div>
+</div>
+
+<footer>
+
+    <p>Derechos reservados &copy; 2023</p>
+</footer>
+
+</body>
+
+</html>
